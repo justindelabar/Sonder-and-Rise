6bbfad8e8c21e3c8d607b98e315b8de76adea1ae
justindelabar <justin.delabar@uc.edu>
2016-09-27 09:12:18 -0400

    Responsive updates for article pages

--- content.php
+++ content.php
@@ -1,6 +1,6 @@
 <?php $thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );?>
 
-<header id="article-header"  style="background-image: url('<?php echo $thumb['0'];?>');">
+<header id="article-header" style="background-image: url('<?php echo $thumb['0'];?>');">
  
 </header>
 
